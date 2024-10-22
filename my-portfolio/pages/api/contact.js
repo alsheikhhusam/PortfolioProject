ba8338fcdd17b27ee0751ace6790c2918681af19
HusamAlsheikh <alsheikhhusam@gmail.com>
2024-10-22 13:25:14 -0400

    Setup Nodemailer
    Now can receive emails from contact form page

--- my-portfolio/pages/api/contact.js
+++ my-portfolio/pages/api/contact.js
@@ -1,12 +1,37 @@
+import nodemailer from 'nodemailer';
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {    //  Only allow POST requests
         const { name, email, message } = req.body;
-        
-        //TODO    simulate sending an email
-        console.log('Contact form submitted:', { name, email, message });
-    
-        //    Return 200
-        res.status(200).json({ success: true, message: 'Form submitted successfully!' });
+
+        //  Create a nodemailer transporter
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.EMAIL_PASSWORD
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
+
+        //  Populate Email
+        const mailOptions = {
+            from: email,
+            to: process.env.RECEIVER_EMAIL,
+            subject: `New Portfolio Contact Form Submission from ${name}`,
+            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+        };
+
+        //  Send Email
+        try {
+            await transporter.sendMail(mailOptions);
+            res.status(200).json({ success: true, message: 'Message sent successfully!' });
+        } catch (error) {
+            console.error('Error sending email:', error);
+            res.status(500).json({ success: false, message: 'Error sending message. Please try again later.' });
+        }
     } else {    //  If not POST then throw 405 error
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
